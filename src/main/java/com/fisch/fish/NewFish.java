8050b46e56b95fd6ed8826b6866039c99a6984f8
DK122323234 <dkamensky@licei10.ru>
2026-06-08 23:25:04 +0300

    Добавил класс новой рыбы

---
+++ src/main/java/com/fisch/fish/NewFish.java
@@ -0,0 +1,19 @@
+package com.fisch.fish;
+
+public class NewFish {
+    String name;
+    int rarity;
+    long weight;
+    String bestBait;
+    String bestWeather;
+    String bestTime;
+
+    public NewFish(String name,int rarity, long weight, String bestBait, String bestWeather, String bestTime) {
+        this.name = name;
+        this.rarity = rarity;
+        this.weight = weight;
+        this.bestBait = bestBait;
+        this.bestWeather = bestWeather;
+        this.bestTime = bestTime;
+    }
+}
