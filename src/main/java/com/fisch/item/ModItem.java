5e8de3cc020099d68be80a049bc3fa5c88bb9ab3
DKanter12 <dkamensky@licei10.ru>
2026-06-21 17:40:23 +0300

    Добавил 5 новых удочек

--- src/main/java/com/fisch/item/ModItem.java
+++ src/main/java/com/fisch/item/ModItem.java
@@ -12,20 +12,20 @@ public class ModItem {
     private static final Item TEST = registerItem("test", new Item(new Item.Properties()));
     private static final NewRod TEST_FISHING_ROD = (NewRod) registerItem("test_rod", new NewRod(new Item.Properties(), 10f, 0.3f, 1f));
 
-    public static final NewRod WOODEN_ROD = (NewRod) registerItem("wooden_rod", 
-            new NewRod(new Item.Properties().durability(100), 15f, 0.1f, 1.0f));
+    public static final NewRod WOODEN_ROD = (NewRod) registerItem("training_rod", 
+            new NewRod(new Item.Properties(), -70f, 0.2f, 20f));
 
-    public static final NewRod LUCKY_ROD = (NewRod) registerItem("lucky_rod", 
-            new NewRod(new Item.Properties().durability(150), 12f, 0.4f, 0.8f));
+    public static final NewRod LUCKY_ROD = (NewRod) registerItem("plastic_rod", 
+            new NewRod(new Item.Properties(), 15f, 0.01f, 10f));
 
-    public static final NewRod PLASTIC_ROD = (NewRod) registerItem("plastic_rod", 
-            new NewRod(new Item.Properties().durability(80), 20f, -0.1f, 1.4f));
+    public static final NewRod PLASTIC_ROD = (NewRod) registerItem("fungal_rod", 
+            new NewRod(new Item.Properties(), 45, 0.01f, 0f));
 
     public static final NewRod TRAINING_ROD = (NewRod) registerItem("training_rod", 
-            new NewRod(new Item.Properties().durability(120), 10f, 0.0f, 1.1f));
+            new NewRod(new Item.Properties(), 10f, 0.0f, 1.1f));
 
     public static final NewRod CARBON_ROD = (NewRod) registerItem("carbon_rod", 
-            new NewRod(new Item.Properties().durability(250), 25f, 0.2f, 1.2f));
+            new NewRod(new Item.Properties(), 25f, 0.05f, 10f));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(
